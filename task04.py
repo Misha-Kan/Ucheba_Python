606ee5699fe9808300db8ca03b74b78fce8b2198
Misha-Kan <kanakov2@narod.ru>
2024-09-11 15:02:40 +0300

    Update task04.py

--- task04.py
+++ task04.py
@@ -1,5 +1,6 @@
 # 4th program
-print((int((float("123.456") - int(123.456)) * 10)))
-
+# print((int((float("123.456") - int(123.456)) * 10)))
+# peredelka
+print((int(float("123.456") * 10))%10)
 
 
