bb583538d37fac5221a41fd421320f1b1c52fc19
Misha-Kan <kanakov2@narod.ru>
2024-09-11 16:01:40 +0300

    Add files via upload

---
+++ peremenniye.py
@@ -0,0 +1,8 @@
+vsego_DZ = 12
+zatrach_chasov = 1.5
+name_kursa = "Python"
+srednee_vremya = zatrach_chasov/vsego_DZ
+# print(srednee_vremya)
+# print(name_kursa * 5)
+
+print("Курс:",name_kursa,", всего задач:",vsego_DZ,", затрачено часов:",zatrach_chasov,", среднее время выполнения",srednee_vremya,"часа.")
