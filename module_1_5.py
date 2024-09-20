ab175a0fee12135307870409e26274d7f843d513
Misha-Kan <kanakov2@narod.ru>
2024-09-20 21:40:47 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,17 @@
+# immutable_var
+immutable_var = (1, 2, "Stroka")
+print(immutable_var)
+print(immutable_var[2])
+# immutable_var[1] = 4
+# закомментил предыдущую строку, иначе ошибка не дает работать проге дальше
+
+print(immutable_var)
+# mutable_list
+mutable_list = [3, 5, True]
+print(mutable_list)
+mutable_list[0] = 2
+mutable_list[1] = 4
+mutable_list[2] = False
+print(mutable_list)
+
+
