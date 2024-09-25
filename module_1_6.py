9cb56df35e8d97dacf9ccaf27bd483363e83c71a
Misha-Kan <kanakov2@narod.ru>
2024-09-25 20:02:27 +0300

    Add files via upload

--- module_1_6.py
+++ module_1_6.py
@@ -1,3 +1,4 @@
+
 print("Словари")
 my_dict = {"Dasha_M": 1988, "Misha_M": 1990}
 print(my_dict)
@@ -7,9 +8,8 @@ my_dict["Lisa_K"] = 1992
 print(my_dict["Lisa_K"])
 my_dict.update({"Olga_K": 1962, "Misha_K": 1965})
 print(my_dict)
-del my_dict["Lisa_K"]
-#print(my_dict)
-print(my_dict.get("Lisa_K", "Клиент не найден"))
+print(my_dict.pop(("Lisa_K")))
+print(my_dict.get("Piter_L"))
 print(my_dict)
 print("Множества")
 my_set = {1, 4, 7, 8, 2, 4, 8, 1}
